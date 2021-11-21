7e8fff46ba5c8697ca4df07c98599429f9a0e68a
Ayaka Miyake <ayakamiyake@miyakeayakanoMacBook-Pro.local>
2021-11-21 16:24:00 +0000

    Change files.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
-import React, { useReducer, createContext, useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import './App.css';
-// import Counter from './components/Counter'
-// import CounterHook from './components/CounterHook'
-// import FormHook from './components/FormHook'
-// import ItemHook from './components/ItemHook'
-// import EffectHook from './components/EffectHook'
-// import MouseEventEffect from './components/MouseEventEffect'
-// import DataFetch from './components/DataFetch'
-// import DataFetchById from './components/DataFetchById'
-// import ComponentC from './components/ComponentC'
-// import CounterReducer from './components/CounterReducer'
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-import ComponentC from './components/ComponentC'
 
-export const CountContext= createContext()
+// import ComponentA from './components/ComponentA'
+// import ComponentB from './components/ComponentB'
+// import ComponentC from './components/ComponentC'
+import axios from 'axios';
 const initialState = {
-  firstCounter: 0
+  loading: true,
+  error: '',
+  post: {}
 }
 const reducer = (state, action) => {
-  switch(action.type){
-      case 'increment1':
-          return { ...state, firstCounter: state.firstCounter + action.value}
-      case 'decrement1':
-          return { ...state, firstCounter: state.firstCounter - action.value}
-      case 'reset':
-          return initialState
-      default:
-          return state
+  switch(action.type) {
+    case 'FETCH_SUCCESS':
+      return {
+        loading: false,
+        post: action.payload,
+        error: ''
+      }
+    case 'FETCH_ERROR':
+      return {
+        loading: false,
+        post: {},
+        error: 'Data failed.'
+      }
+    default:
+      return state
   }
 }
+
+// export const CountContext= createContext()
+// const initialState = {
+//   firstCounter: 0
+// }
+// const reducer = (state, action) => {
+//   switch(action.type){
+//       case 'increment1':
+//           return { ...state, firstCounter: state.firstCounter + action.value}
+//       case 'decrement1':
+//           return { ...state, firstCounter: state.firstCounter - action.value}
+//       case 'reset':
+//           return initialState
+//       default:
+//           return state
+//   }
+// }
 
 // export const UserContext = createContext()
 // export const LanguageContext = createContext()
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   // const [user, setUser] = useState({name: 'yamada', age: '32'})
   // const [language, setLanguage] = useState('日本語')
 
-  const [count, dispatch] = useReducer(reducer, initialState)
-
+  useEffect(() => {
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => {
+      dispatch({type: 'FETCH_SUCCESS', payload: res.data})
+    })
+    .catch(err => {
+      dispatch({type: 'FETCH_ERROR'})
+    })
+  })
   return (
     <div className="App">
       {/* <Counter /> */}
@@ -56,12 +81,16 @@ function App() {
         </LanguageContext.Provider>
       </UserContext.Provider> */}
       {/* <CounterReducer /> */}
-      <h1>カウント: {count.firstCounter}</h1>
+      {/* <h1>カウント: {count.firstCounter}</h1>
       <CountContext.Provider value={{ countState: count, countDispatch: dispatch }} >
         <ComponentA />
         <ComponentB />
         <ComponentC />
-      </CountContext.Provider>
+      </CountContext.Provider> */}
+
+      <h1>{state.loading ? 'Loading...' : state.post.title}</h1>
+      <h2>{state.error ? state.error: null}</h2>
+
 
 
     </div>
--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import {CountContext} from '../App'
 
 function ComponentA() {
-    const CountContext = useContext(CountContext)
+    const countContext = useContext(CountContext)
     return (
         <div>
-            <button onClick={() => CountContext.countDispatch({type: 'increment1', value: 1})}>increment1</button>
-            <button onClick={() => CountContext.countDispatch({type: 'decrement1', value: 1})}>decrement1</button>
-            <button onClick={() => CountContext.countDispatch({type: 'reset'})}>Reset</button>
+            <button onClick={() => countContext.countDispatch({type: 'increment1', value: 1})}>increment1</button>
+            <button onClick={() => countContext.countDispatch({type: 'decrement1', value: 1})}>decrement1</button>
+            <button onClick={() => countContext.countDispatch({type: 'reset'})}>Reset</button>
         </div>
     )
 }
